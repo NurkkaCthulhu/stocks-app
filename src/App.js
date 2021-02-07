29190501754a00e07e57535c6309cd4bc335acb0
NurkkaCthulhu <nurkkacthulhu@gmail.com>
2021-02-08 00:14:46 +0200

    Add loading spinner, loading variable.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import './App.css';
-import  React, { useState, useEffect } from 'react';
+import  React, { useState } from 'react';
 import CSVloader from "./CSVloader";
 import StocksTable from "./StocksTable";
 
 export default function App() {
 
-  const [stocks, setStocks] = useState([])
-  const [csvTitles, setCsvTitles] = useState([])
+  const [stocks, setStocks] = useState([]);
+  const [csvTitles, setCsvTitles] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   return (
     <div className="App">
       <h1>Statistics about Stock Prices</h1>
-      <p>This stocks app follows Nasdaq's CSV file specifications. This means that the data should be in the “Date, Close/Last, Volume, Open, High, Low" format. Start exploring stock statistics by loading a CSV file.</p>
-      <CSVloader importFileContents={setStocks} getCSVTitles/>
-      <StocksTable stocks={stocks}/>
+      <p>This stocks app follows Nasdaq's CSV file specifications. This means that the data should be in <br/><i>Date, Close/Last, Volume, Open, High, Low</i><br/>format. Start exploring stock statistics by loading a CSV file.</p>
+      <CSVloader importFileContents={setStocks} setCsvTitles={setCsvTitles} setLoading={setLoading}/>
+      {loading ?
+        <div>
+          <div className="loader"></div>
+          <p>Loading...</p>
+        </div>
+        :
+        <StocksTable stocks={stocks} csvTitles={csvTitles}/>
+      }
     </div>
   );
 }
