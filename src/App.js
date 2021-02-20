302843032d6ca1f0a28ec6e2122707d6b96cc6db
NurkkaCthulhu <nurkkacthulhu@gmail.com>
2021-02-20 19:51:54 +0200

    Add Nav, QuestionAnswers components to view.
    Also add variable for current view.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import CSVloader from "./components/CSVloader.js";
-import StocksTable from "./components/StocksTable.js";
 import DateRangePicker from "./components/DateRangePicker";
+import Nav from "./components/Nav";
+import QuestionAnswer from "./components/QuestionAnswer";
 import './css/App.css';
 import "react-datepicker/dist/react-datepicker.css";
+
+
 
 export default function App() {
 
@@ -12,6 +15,7 @@ export default function App() {
   const [loading, setLoading] = useState(false);
   const [startDate, setStartDate] = useState(new Date(2000, 1, 1));
   const [endDate, setEndDate] = useState(new Date());
+  const [activeView, setActiveView] = useState("TABLE");
   const stockTitles = "Date, Close/Last, Volume, Open, High, Low";
 
   return (
@@ -21,13 +25,14 @@ export default function App() {
       <i>Date, Close/Last, Volume, Open, High, Low</i><br/>format. Start exploring stock statistics by loading a CSV file.</p>
       <CSVloader exportFileContents={setStocks} exportCsvTitles={setCsvTitles} setLoading={setLoading} fileHeader={stockTitles} />
       <DateRangePicker startDate={startDate} setStartDate={setStartDate} endDate={endDate} setEndDate={setEndDate}/>
+      <Nav changeViewTo={setActiveView} currentView={activeView} />
       {loading ?
         <div>
           <div className="loader"></div>
           <p>Loading...</p>
         </div>
         :
-        <StocksTable stocks={stocks} csvTitles={csvTitles}/>
+        <QuestionAnswer stocks={stocks} csvTitles={csvTitles} view={activeView}/>
       }
     </div>
   );
