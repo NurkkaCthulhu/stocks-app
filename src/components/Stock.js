6fe206c247e67b8855847467e9593601fbdb6f77
NurkkaCthulhu <nurkkacthulhu@gmail.com>
2021-02-22 00:45:43 +0200

    Add Stock class.

---
+++ src/components/Stock.js
@@ -0,0 +1,10 @@
+export default class Stock {
+  constructor( date, close, volume, open, high, low ) {
+    this.date = date;
+    this.close = close;
+    this.volume = volume;
+    this.open = open;
+    this.high = high;
+    this.low = low;
+  }
+}
