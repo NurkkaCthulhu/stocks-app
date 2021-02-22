b62b4c6fc941524b5fe8b503331b530ae0d50ae1
NurkkaCthulhu <nurkkacthulhu@gmail.com>
2021-02-22 21:06:55 +0200

    Fix bug that errored the app when A was visited before stocks were loaded.

--- src/components/AnswerA.js
+++ src/components/AnswerA.js
@@ -11,16 +11,26 @@ import Stock from "./Stock";
 
 export default function AnswerA({startDate, endDate, stocks}) {
 
-  function calculateBullishTrend() {
-
+  function getSortedStocks() {
     let sortedStocks = [];
-    for (let i = 0; i < stocks.length-1; i++) {
+    for (let i = 0; i < stocks.length; i++) {
       let splitStock = stocks[i].split(',');
-      let oneStock = new Stock(new Date(splitStock[0]), parseFloat(splitStock[1].substring(2)), parseInt(splitStock[2].substring(1)),
-        parseFloat(splitStock[3].substring(2)), parseFloat(splitStock[4].substring(2)), parseFloat(splitStock[5].substring(2)));
+      let oneStock = new Stock(
+        new Date(splitStock[0]),
+        parseFloat(splitStock[1].replace('$', '')),
+        parseInt(splitStock[2]),
+        parseFloat(splitStock[3].replace('$', '')),
+        parseFloat(splitStock[4].replace('$', '')),
+        parseFloat(splitStock[5].replace('$', '')));
       sortedStocks.push(oneStock);
     }
+
     sortedStocks.sort((a, b) => (a.date > b.date) ? 1 : -1);
+    return sortedStocks;
+  }
+
+  function calculateBullishTrend() {
+    const sortedStocks = getSortedStocks();
 
     let longestTrend = 0;
     let currentTrendLength = 0;
@@ -45,14 +55,16 @@ export default function AnswerA({startDate, endDate, stocks}) {
 
     return (
       <div id="bullish">
-        <p>LONGEST BULLISH TREND WAS {longestTrend}</p>
+        <p>The longest bullish trend was {longestTrend}</p>
       </div>
     )
   }
 
   return (
     <div id="answer-a">
-      {stocks && calculateBullishTrend()}
+      {stocks.length > 0 ? calculateBullishTrend() :
+        <p>Load some stocks to see the longest bullish trend</p>
+      }
     </div>
   )
 }
