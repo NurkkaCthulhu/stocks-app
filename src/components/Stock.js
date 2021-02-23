e298929c42124424830ed17f22b31184f3ec7005
NurkkaCthulhu <nurkkacthulhu@gmail.com>
2021-02-23 23:48:33 +0200

    Add documentation to Stock.

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,3 +1,13 @@
+/**
+ *  Stock class is used to easily access stock information. Instead of class the stock could have been an array,
+ *  but it's easier to ask for example highest with stock.high than stock[5].
+ *  @param {Date} date      - Date from which the stock data is from
+ *  @param {float} close    - The price of the stock at market closing
+ *  @param {int} volume     - Number of stocks traded
+ *  @param {float} open     - The price of the stock at market opening
+ *  @param {float} high     - The highest price of the stock during the day
+ *  @param {float} low      - The lowest price of the stock during the day
+ */
 export default class Stock {
   constructor( date, close, volume, open, high, low ) {
     this.date = date;
